057509459a8067d1919e7ff21d45a16fea24c48b
Shuimo03 <wu1998102@gmail.com>
2020-07-09 14:49:54 +0800

    vue脚手架

---
+++ wr/src/main/java/worldroot/wr/WrApplication.java
@@ -0,0 +1,13 @@
+package worldroot.wr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrApplication.class, args);
+	}
+
+}
---
+++ wr/src/test/java/worldroot/wr/WrApplicationTests.java
@@ -0,0 +1,13 @@
+package worldroot.wr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
